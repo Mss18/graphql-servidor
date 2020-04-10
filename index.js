99619975075f5a199916b1d3e3289b879a6b0460
Mss18 <mssonfire@gmail.com>
2020-04-10 13:45:13 +0200

    resolver y primer mutation

--- index.js
+++ index.js
@@ -10,9 +10,33 @@ app.get('/', (req, res) => {
     res.send('Todo listo');
 });
 
+class Cliente {
+    constructor(id, {nombre, apellido, empresa, email}) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.empresa = empresa;
+        this.email = email;
+    }
+}
+
+const clientesDB = {};
+
 //resolver
 const root = {
-    hola: () => 'Hola mundo desde GraphQL'
+    cliente: () => {
+      return {
+        "id": 123456789,
+        "nombre": "Martin",
+        "apellido": "Serantes",
+        "empresa": "Free",
+        "email": "correo@correo.com"}
+    },
+    crearCliente: ({input}) => {
+        const id = require('crypto').randomBytes(10).toString('hex');
+        clientesDB[id] = input;
+        return new Cliente(id, input);
+    }
 };
 
 app.use('/graphql', graphqlHTTP({
--- schema.js
+++ schema.js
@@ -1,8 +1,28 @@
 import {buildSchema} from 'graphql';
 
 const schema = buildSchema(`
+    type Cliente {
+        id: ID
+        nombre: String
+        apellido: String
+        empresa: String
+        email: String
+    }
     type Query {
-        hola: String
-    }`);
+        cliente: Cliente
+    }
+    input ClienteInput {
+        id: ID
+        nombre: String
+        apellido: String
+        empresa: String
+        email: String
+    }
+    type Mutation {
+        crearCliente(input:
+        ClienteInput):
+        Cliente
+    }
+    `);
 
     export default schema;
